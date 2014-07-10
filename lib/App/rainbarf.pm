334816ac4abb8981f38ba60bb27ce75009d66a0c
Stanislaw Pusep <creaktive@gmail.com>
2014-07-10 10:04:52 +0200

    Version bump

--- lib/App/rainbarf.pm
+++ lib/App/rainbarf.pm
@@ -3,7 +3,7 @@ package App::rainbarf;
 use strict;
 use warnings;
 
-our $VERSION = q(1.1);
+our $VERSION = q(1.2);
 
 1;
 
@@ -16,7 +16,7 @@ rainbarf - CPU/RAM/battery stats chart bar for tmux (and GNU screen)
 
 =head1 VERSION
 
-version 1.1
+version 1.2
 
 =head1 DESCRIPTION
 
